5787dabe93ec578ee440874f9fac25d8713ad0de
Tectone <tectone18@gmail.com>
2018-11-22 16:33:21 -0500

    dev.jrr.1stWave
    posting progress for possible checkpoint tomorrow

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'includes/header.html';?>
+
+
+<body class="animated fadeIn">
+
+<div class="container">
+    <img class="logo" src="images/rokuLogo.png" alt="Roku Flashback Logo"> 
+    <div class="navigation">
+    </div>
+    
+
+
+</div>
+
+    <script src="js/app.js"></script>                  
+    <script src="js/foundation.min.js"></script>
+    <script>  $(document).foundation(); </script>
+    </body></html>
